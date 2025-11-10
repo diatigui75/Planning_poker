acbd82125172d1f124ddd05dfead5873afbbc994
Melissa Aliouche <maliouche@univ-lyon2.fr>
2025-11-10 15:53:15 +0100

    Ajout des fichiers et commentaires JSDoc pour la documentation

--- src/game.js
+++ src/game.js
@@ -1,5 +1,13 @@
+
 import { unanimous, average, median } from './rules.js';
 
+/**
+ * Calcule le résultat final des votes selon la règle choisie.
+ * @param {Array<number>} votes - Liste des votes des joueurs
+ * @param {string} rule - La règle de calcul ('unanimity', 'average', 'median')
+ * @returns {number|null} Le résultat calculé
+ * @throws {Error} Si la règle est inconnue
+ */
 export function computeResult(votes, rule) {
   switch (rule) {
     case 'unanimity': return unanimous(votes);
