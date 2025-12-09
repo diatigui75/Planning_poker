ea5ab820cf0cc4ce569ca36a0b7b05f4a14c467b
Aliouche Melissa <aliouchemelissa0@gmail.com>
2025-12-09 17:09:35 +0100

    Ajout d'un exemple de configuration pour GitHub

---
+++ config/config_exemple.php
@@ -0,0 +1,10 @@
+<?php
+// exemple du fichier config.php qui contient des infos sensibles
+return [
+    'db_host' => 'localhost',  
+    'db_port' => 3306,    // ou le port de MySQL sur votre machine  
+    'db_name' => 'planning_poker',
+    'db_user' => 'root',
+    'db_pass' => 'password',    
+    'base_url' => '/Planning_poker/public',
+];
